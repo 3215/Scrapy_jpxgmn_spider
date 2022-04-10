21ae910890cb3222af12995c3c1f3b813661c4bf
3215 <2423980979@qq.com>
2022-04-10 13:46:04 +0800

    更新目标地址

--- jpxgmn_spider/spiders/jpxgmn.py
+++ jpxgmn_spider/spiders/jpxgmn.py
@@ -5,23 +5,25 @@ from ..items import JpxgmnSpiderItem
 
 class JpxgmnSpider(scrapy.Spider):
     name = 'jpxgmn'
-    allowed_domains = ['www.jpxgmn.net']
-    start_urls = ['http://www.jpxgmn.net/']
-    resource_url = 'https://p.jpxgmn.net/'
+    allowed_domains = ['www.jpxgyw.net']
+    start_urls = ['http://www.jpxgyw.net/']
+    resource_url = 'https://p.jpxgyw.net/'
+    header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36 Edg/100.0.1185.36'}
 
     def parse(self, response):
         # 通过主站获取各个系列的 url
         menu_item = response.xpath("//ul[@class='sub-menu']//*[@class='menu-item']/a/@href").extract()[:1]
         for item in menu_item:
             item_url = parse.urljoin(response.url, item)
-            yield scrapy.Request(url=item_url, callback=self.parse_1)   # 传给 parse_1 回调函数进行进一步处理
+
+            yield scrapy.Request(url=item_url, headers=self.header, callback=self.parse_1, dont_filter=True)   # 传给 parse_1 回调函数进行进一步处理
 
     def parse_1(self, response):
         # 通过系列页面获取各个图组的 url
         related_box = response.xpath("//*[@class='related_box']/a/@href").extract()[:3]    # [:5]调节爬取页面数量
         for box in related_box:
             box_url = parse.urljoin(response.url, box)
-            yield scrapy.Request(url=box_url, callback=self.find_img_url)   # 传给 find_img_url 回调函数进行解析
+            yield scrapy.Request(url=box_url, headers=self.header, callback=self.find_img_url, dont_filter=True)   # 传给 find_img_url 回调函数进行解析
 
         # next_page = response.xpath("//a[contains(text(), '下一页')]/@href").extract_first('')
         # yield scrapy.Request(url=parse.urljoin(response.url, next_page), callback=self.parse_1)
@@ -31,7 +33,7 @@ class JpxgmnSpider(scrapy.Spider):
         image_info = JpxgmnSpiderItem()    # 实例化 item 对象
         image_info["image_paths"] = response.xpath("//*[@class='article-title']/text()").extract_first('')
 
-        img_urls = response.xpath("//img[@onload='size(this)']/@src").extract()
+        img_urls = response.xpath("//p/img/@src").extract()
         image_urls = []    # 待下载的图片url列表（必须为列表）
         for img_url in img_urls:
             img_url = '/U' + img_url[2:]
